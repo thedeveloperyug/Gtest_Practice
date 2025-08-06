7636561ec84c8ecf5dcd462438f5dad0768c6d5d
yogeshpandey <Yogesh.Pandey@bti.bmwgroup.com>
2025-08-06 12:39:45 +0530

    Level 1

---
+++ Basics/lec_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <gtest/gtest.h>
+using namespace std;
+
+
+// Passed when comparision is True
+TEST(TestName, Subtest_1){
+    ASSERT_TRUE(1==1);
+}
+
+
+// Passed when comparision is false
+TEST(TestName, Subtest_2){
+    ASSERT_FALSE(1==2);
+}
+
+// Basic Structure of Gtest
+int main(int argc,char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
