5ad212a6fe8c99eb87a65370967daa2459e37a9f
sonu1 <Sonu.1@bti.bmwgroup.com>
2025-08-06 13:38:23 +0530

    fib_project

---
+++ fib_project/test/fib_test.cc
@@ -0,0 +1,12 @@
+#include "fib/fib.h"
+#include <gtest/gtest.h>
+
+TEST(FibTest, HandleCornerCases){
+    EXPECT_EQ(fib(0),0);
+    EXPECT_EQ(fib(1), 1);
+}
+
+TEST(FibTest, HandleGeneralCases){
+    EXPECT_EQ(fib(5),5);
+    EXPECT_EQ(fib(10),55);
+}
